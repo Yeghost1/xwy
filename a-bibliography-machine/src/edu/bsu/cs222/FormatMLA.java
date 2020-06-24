7a449f5cb5ba09549b14417abf4f763537f34286
Janardhen <52244557+Janardhen@users.noreply.github.com>
2020-06-23 23:16:40 -0400

    added test to formatMLA

--- a-bibliography-machine/src/edu/bsu/cs222/FormatMLA.java
+++ a-bibliography-machine/src/edu/bsu/cs222/FormatMLA.java
@@ -2,12 +2,10 @@ package edu.bsu.cs222;
 
 //This class formats the info from QueryCitationInfo into MLA format.
 
-public class FormatMLA extends QueryCitationInfo {
+public class FormatMLA extends QueryCitationInfo{
 
-    public static String formatMLA() {
-        String result =(authorName + ". \"" + articleName + "\" " + publisherName + ", " + publicationDate + ", " + url + ". Accessed " + dateAccessed);
+    public static String formatMLA(){
+        String result = (authorName + ". \"" + articleName + "\" " + publisherName + ", " + publicationDate + ", " + url + ". Accessed " + dateAccessed);
         return result;
     }
-
-
 }
